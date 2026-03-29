1126a982fbf170fb6f0e2f3b99c363c7b243f112
riddler265 <egoor120@gmail.com>
2026-03-30 01:39:49 +0300

    Готовый код. Готовый отчет.

--- lab1/script.sql
+++ lab1/script.sql
@@ -1,9 +1,15 @@
 BEGIN;
 
-CREATE TYPE sex AS ENUM ('man', 'woman');
+--дропнуть перед началом
+DROP TABLE IF EXISTS entities_action, entity, action, location, human, ship CASCADE;
+DROP TYPE IF EXISTS sex, major CASCADE;
 
+--СОЗДАНИЕ
+--енамчики
+CREATE TYPE sex AS ENUM ('man', 'woman');
 CREATE TYPE major AS ENUM ('captain', 'engineer', 'cook', 'cleaner');
 
+--таблицы
 CREATE TABLE ship (
     id SERIAL PRIMARY KEY,
     name TEXT,
@@ -46,3 +52,44 @@ CREATE TABLE entities_action (
     entity_id INTEGER NOT NULL REFERENCES entity(id),
     action_id INTEGER NOT NULL REFERENCES action(id)
 );
+
+--ЗАПОЛНЕНИЕ
+INSERT INTO ship (name, description) VALUES
+('Покойоколка', 'Загадочное судно с хранителями свэга в синих кепках'),
+('67-й покоритель', 'Давным-давно утерянная среди песков времени легенда'),
+('Т.Ю.', 'Летающая обитель космо-варваров');
+
+INSERT INTO human (name, age, major, sex, ship_id) VALUES
+('Егор', 52, 'cook', DEFAULT, 1),
+('Боб', 67, 'cleaner', DEFAULT, 3),
+('Угара', 1488, NULL, 'woman', 3),
+('Последний выживший', 1000000000, 'captain', DEFAULT, 2);
+
+INSERT INTO location (name, description)
+VALUES ('Олимп', 'Место, где встречаются легенды космического дрейфа');
+
+INSERT INTO entity (name, description) VALUES
+('Трактор', 'Наиужаснейший монстр из всех существовавших'),
+('Карман', 'Правая рука Трактора');
+
+INSERT INTO action (name, description, ship_id, location_id, time, result) VALUES 
+('Прах Покойоколки', 'В первые моменты Олимпийской резни корабль Покойоколка обрел вечный покой', 1, 1, '2976-01-18 15:28:00', TRUE),
+('Ликбез', 'Не успел никто опомнится после Праха Покойоколки, как тут же судно Т.Ю погрузилось в пасть Кармана', 3, 1, '2976-01-18 15:30:47', TRUE),
+('Покоритель покорил', 'Единственным выжившим при Олимпе стал капитан 67-го покорителя', 2, 1, '2976-01-18 16:02:14', FALSE);
+
+INSERT INTO entities_action (entity_id, action_id) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 2),
+(2, 3);
+
+--ВЫВОД
+SELECT * FROM human;
+SELECT * FROM ship;
+SELECT * FROM action;
+SELECT * FROM entity;
+SELECT * FROM entities_action;
+SELECT * FROM location;
+
+COMMIT;
